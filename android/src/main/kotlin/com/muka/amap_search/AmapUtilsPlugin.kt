d49d05cf30c11129d99cc8f13ec6782aaad1178d
Spicely <Spicely@outlook.com>
2020-06-08 15:49:38 +0800

    []

--- android/src/main/kotlin/com/muka/amap_search/AmapUtilsPlugin.kt
+++ android/src/main/kotlin/com/muka/amap_search/AmapUtilsPlugin.kt
@@ -1,8 +1,8 @@
 package com.muka.amap_search
 
-import androidx.annotation.NonNull;
-import com.amap.api.location.CoordinateConverter
+import androidx.annotation.NonNull
 import com.amap.api.maps.AMapUtils
+import com.amap.api.location.CoordinateConverter
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -11,40 +11,22 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-/** AmapSearchPlugin */
-public class AmapSearchPlugin : FlutterPlugin, MethodCallHandler {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
+/** AmapUtilsPlugin */
+public class AmapUtilsPlugin : FlutterPlugin, MethodCallHandler {
     private lateinit var channel: MethodChannel
-    private lateinit var utilsChannel: MethodChannel
     private lateinit var pluginBinding: FlutterPlugin.FlutterPluginBinding
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         pluginBinding = flutterPluginBinding
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "plugins.muka.com/amap_search")
-        channel.setMethodCallHandler(this)
-        utilsChannel = MethodChannel(flutterPluginBinding.binaryMessenger, "plugins.muka.com/amap_utils")
-        utilsChannel.setMethodCallHandler(this);
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "plugins.muka.com/amap_utils")
+        channel.setMethodCallHandler(this);
     }
 
-    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-    // plugin registration via this function while apps migrate to use the new Android APIs
-    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-    //
-    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-    // in the same class.
     companion object {
         @JvmStatic
         fun registerWith(registrar: Registrar) {
-            val channel = MethodChannel(registrar.messenger(), "amap_search")
-            channel.setMethodCallHandler(AmapSearchPlugin())
-            val utilsChannel = MethodChannel(registrar.messenger(), "plugins.muka.com/amap_utils")
-            utilsChannel.setMethodCallHandler(AmapSearchPlugin());
+            val channel = MethodChannel(registrar.messenger(), "plugins.muka.com/amap_utils")
+            channel.setMethodCallHandler(AmapUtilsPlugin())
         }
     }
 
@@ -84,6 +66,5 @@ public class AmapSearchPlugin : FlutterPlugin, MethodCallHandler {
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
-        utilsChannel.setMethodCallHandler(null)
     }
 }
--- android/src/main/kotlin/com/muka/amap_search/Convert.kt
+++ android/src/main/kotlin/com/muka/amap_search/Convert.kt
@@ -1,4 +1,5 @@
 package com.muka.amap_search
+import com.amap.api.location.DPoint
 import com.amap.api.maps.model.LatLng
 
 class Convert {
@@ -24,6 +25,24 @@ class Convert {
 
         fun toDouble(o: Any): Double {
             return o as Double
+        }
+
+        fun toDPoint(o: Any?): DPoint? {
+            if (o == null) {
+                return null
+            }
+            var data = toMap(o)
+            if (data["latitude"] != null && data["longitude"] != null) {
+                return DPoint(data["latitude"] as Double, data["longitude"] as Double)
+            }
+            return null
+        }
+
+        fun toJson(point: DPoint): Any {
+            val data = HashMap<String, Any>()
+            data["latitude"] = point.latitude
+            data["longitude"] = point.longitude
+            return data
         }
     }
 }
