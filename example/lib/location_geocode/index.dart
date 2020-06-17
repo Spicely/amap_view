467a45310b994a2b1489053d53d3f89ccd04f9ce
Spicely <Spicely@outlook.com>
2020-06-17 17:25:16 +0800

    增加IOS poi数据

---
+++ example/lib/location_geocode/index.dart
@@ -0,0 +1,38 @@
+import '../location_pois/index.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'index.g.dart';
+
+@JsonSerializable()
+class LocationGeocode {
+  final String towncode;
+
+  final String township;
+
+  final String adCode;
+
+  final String cityCode;
+
+  final String country;
+
+  final String formatAddress;
+
+  final String province;
+
+  List<LocationPois> pois  = [];
+
+  LocationGeocode({
+    this.adCode,
+    this.cityCode,
+    this.country,
+    this.formatAddress,
+    this.province,
+    this.towncode,
+    this.township,
+    this.pois,
+  });
+
+  factory LocationGeocode.fromJson(Map<dynamic, dynamic> json) => _$LocationGeocodeFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LocationGeocodeToJson(this);
+}
