64c2a4a7435a1858f92152e0692be8bb29ae710e
Spicely <spicely@outlook.com>
2020-11-18 18:04:41 +0800

    []

--- android/src/main/kotlin/com/muka/amap_location/AmapLocationPlugin.kt
+++ android/src/main/kotlin/com/muka/amap_location/AmapLocationPlugin.kt
@@ -1,14 +1,15 @@
 package com.muka.amap_location
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
+import android.annotation.SuppressLint
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Build
+import android.os.IBinder
+import android.os.PowerManager
+import android.util.Log
 import androidx.annotation.NonNull
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
@@ -25,7 +26,7 @@ import kotlin.random.Random
 
 
 /** AmapLocationPlugin */
-public class AmapLocationPlugin : FlutterPlugin, MethodCallHandler, EventChannel.StreamHandler {
+public class AmapLocationPlugin : Service(), FlutterPlugin, MethodCallHandler, EventChannel.StreamHandler {
 
     private lateinit var channel: MethodChannel
     private lateinit var eventChannel: EventChannel
@@ -35,6 +36,8 @@ public class AmapLocationPlugin : FlutterPlugin, MethodCallHandler, EventChannel
     private var channelId: String = "plugins.muka.com/amap_location_server"
     private var notificationManager: NotificationManager? = null
     private var isCreateChannel = false
+    private var msgId = -1
+    private var wakeLock: PowerManager.WakeLock? = null
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
@@ -125,6 +128,32 @@ public class AmapLocationPlugin : FlutterPlugin, MethodCallHandler, EventChannel
         }
     }
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        var flags = flags
+        if (intent != null) {
+            msgId = intent.getIntExtra("msgId", -1)
+        }
+        flags = START_STICKY
+//        setAppBackgroundPlayer()
+        acquireWakeLock()
+        // 刷新定位
+        if (watchClient != null && watchClient.isStarted) {
+            watchClient.startLocation()
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    @SuppressLint("ServiceCast")
+    private fun acquireWakeLock() {
+        if (null == wakeLock) {
+            val pm: PowerManager = getSystemService(POWER_SERVICE) as PowerManager
+            wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK or PowerManager.ON_AFTER_RELEASE, javaClass.canonicalName)
+            wakeLock?.acquire()
+        }
+    }
+
+
+    @SuppressLint("ServiceCast")
     private fun buildNotification(title: String, label: String, name: String, vibrate: Boolean): Notification? {
         var builder: Notification.Builder? = null
         var notification: Notification? = null
@@ -195,5 +224,9 @@ public class AmapLocationPlugin : FlutterPlugin, MethodCallHandler, EventChannel
 
     override fun onCancel(arguments: Any?) {
 
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
     }
 }
