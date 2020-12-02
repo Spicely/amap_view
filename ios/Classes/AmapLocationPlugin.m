97c24e8a4c4e30f6e040aa557571a419e1110235
Spicely <Spicely@outlook.com>
2020-12-02 11:11:44 +0800

    修复更名后ios报错问题

--- ios/Classes/AmapLocationPlugin.m
+++ ios/Classes/AmapLocationPlugin.m
@@ -1,11 +1,11 @@
 #import "AmapLocationPlugin.h"
-#if __has_include(<amap_location/amap_location-Swift.h>)
-#import <amap_location/amap_location-Swift.h>
+#if __has_include(<amap_location_muka/amap_location_muka-Swift.h>)
+#import <amap_location_muka/amap_location_muka-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "amap_location-Swift.h"
+#import "amap_location_muka-Swift.h"
 #endif
 
 @implementation AmapLocationPlugin
